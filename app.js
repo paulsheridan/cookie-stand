5b45a613e83584dd95c9aadc2578837a38fa33c0
Paul Sheridan <paul.sheridan@me.com>
2015-12-08 09:19:44 -0800

    Table is still hideous but it's displaying the correct information in relatively the correct places.  styleTable.css added as a css just for the table.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var hours = [
-  "10am",
-  "11am",
-  "12pm",
-  "1pm",
-  "2pm",
-  "3pm",
-  "4pm",
-  "5pm"
+  "Total",
+  "10a",
+  "11a",
+  "12p",
+  "1p",
+  "2p",
+  "3p",
+  "4p",
+  "5p"
 ];
 
 function CookieStand (locName, minCust, maxCust, avgCookies){
@@ -23,7 +24,7 @@ CookieStand.prototype.customerRand = function(min, max){
 }
 
 CookieStand.prototype.generateHourly = function(){
-  for (i = 0; i < hours.length; i++){
+  for (i = 1; i < hours.length; i++){
     cookiesThisHour = this.customerRand(this.minCust, this.maxCust) * this.avgCookies;
     this.hourlyCookies.push(cookiesThisHour);
     this.dailyCookies += cookiesThisHour;
@@ -32,18 +33,21 @@ CookieStand.prototype.generateHourly = function(){
 
 CookieStand.prototype.render = function(){
   var hoursElem = document.getElementById("hours");
-  var ulElem = document.createElement("ul");
-  ulElem.appendChild(document.createTextNode(this.locName));
+  var tblElem = document.createElement("table");
+  var thElem = document.createElement("th");
+  thElem.textContent = this.locName;
+  tblElem.appendChild(thElem);
+
+  var totElem = document.createElement("td");
+  totElem.textContent = Math.round(this.dailyCookies);
+  tblElem.appendChild(totElem);
+  hoursElem.appendChild(tblElem);
 
   for (i = 0; i < this.hourlyCookies.length; i++){
-    var liElem = document.createElement("li");
-    liElem.textContent = hours[i] + " - " + Math.round(this.hourlyCookies[i]);
-    ulElem.appendChild(liElem);
+    var thElem = document.createElement("td");
+    thElem.textContent = Math.round(this.hourlyCookies[i]);
+    tblElem.appendChild(thElem);
   }
-  var totElem = document.createElement("li");
-  totElem.textContent = "Total - " + Math.round(this.dailyCookies);
-  ulElem.appendChild(totElem);
-  hoursElem.appendChild(ulElem);
 }
 
 var pikePlace = new CookieStand ("Pike Place", 17, 88, 5.2);
@@ -51,6 +55,23 @@ var seaTac = new CookieStand ("SeaTac Airport", 6, 44, 1.2);
 var southCenter = new CookieStand ("Southcenter Mall", 11, 38, 1.9);
 var bellevue = new CookieStand ("Bellevue Mall", 20, 48, 3.3);
 var alki = new CookieStand ("Alki", 3, 24, 2.6);
+
+function firstLine(){
+  var hoursElem = document.getElementById("hours");
+  var tblElem = document.createElement("table");
+  var thElem = document.createElement("th");
+  thElem.textContent = "Locations";
+  tblElem.appendChild(thElem);
+
+  for (i = 0; i < hours.length; i++){
+    var thElem = document.createElement("th");
+    thElem.textContent = hours[i];
+    tblElem.appendChild(thElem);
+    hoursElem.appendChild(tblElem)
+  }
+}
+
+firstLine();
 
 pikePlace.generateHourly();
 pikePlace.render();
